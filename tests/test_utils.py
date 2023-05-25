93d0288f083d6f1dd9310b56fb36c7409d2f1443
Helen Morris <morris_helen@hotmail.com>
2023-05-25 11:02:53 +0100

    write tests get prefix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,7 @@
 from src.utils import get_prefix
 
-def test_returns_empty_string_if_passed_empty_string():
+def test_get_prefix_returns_empty_string_if_passed_empty_string():
     assert get_prefix("") == ""
+def test_get_prefix_returns_prefix():
+    assert get_prefix("10.1080/09500693.2022.2164474") == "10.1080"
+    assert get_prefix("10.1037/a0040251") == "10.1037"
