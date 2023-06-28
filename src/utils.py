29af7ec6c0dcb0a45907df37fe250d974e94030d
Helen Morris <morris_helen@hotmail.com>
2023-06-28 20:10:58 +0100

    add get all publisher names function

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import requests
 import xml.etree.ElementTree as ET
 from os import environ
+import pandas as pd
+import json
 
 load_dotenv()
 
@@ -32,7 +34,22 @@ def get_publisher(doi, filename):
     else:
         print(f'Unable to get publisher. Received response status {response.status_code}')
 
+def get_all_publishers():
+    endpoint = 'http://doi.crossref.org/getPrefixPublisher/'
+    header = {}
+    header['email'] = environ.get('MY_EMAIL')
+    header['user'] = environ.get('USER')
 
+    response = requests.get(endpoint, params={'prefix': 'all'}, headers=header)
 
-get_publisher('10.1037/a0040251', 'test-data.xml')
+    with open('publisher_names.json', 'w') as f:
+        json.dump(response.json(), f, indent=4)
+
+def tidy_csv(filename):
+    data = pd.read_csv(filename)
+    doi_df = pd.DataFrame(data=data)
+    doi_df = doi_df.dropna(axis=0)
+    doi_df.to_csv(filename)
+
+get_all_publishers()
 
