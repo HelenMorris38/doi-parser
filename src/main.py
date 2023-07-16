4d280952ad36b17028c7a8cc056c63527a72d52f
Helen Morris <morris_helen@hotmail.com>
2023-07-16 11:31:55 +0100

    check if json publisher names exists and load data

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from os import path
+from src.utils import get_all_publishers
+import json
 
 def add_publisher_name(filename):
     """Takes a filepath to a csv file. The file must have a column named 'DOI'. It parses the doi prefix and updates the csv file with additional columns: doi_prefix, publisher_name.
@@ -12,5 +15,13 @@ def add_publisher_name(filename):
     print(doi_df.head())
     doi_df['prefix'] = doi_df['DOI'].str.split('/').str[0]
     print(doi_df.head())
+    
+    if not path.isfile('publisher_names.json'):
+        get_all_publishers()
+    
+    with open('publisher_names.json') as f:
+        publisher_names = json.load(f)
+    
+
 
 add_publisher_name('dois.csv')
